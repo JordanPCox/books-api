10f4ed759ff3bf2d1f343d86f8128ea17485a93e
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-02-09 10:56:14 -0500

    Seeded data and passed route tests

--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -2,6 +2,44 @@ const express = require('express')
 const books = express.Router()
 const Book = require('../models/book.js')
 
+// Seed
+books.get('/seed', (req, res) => {
+        Book.insertMany([{
+            "title": "The Shinobi Initiative",
+            "description": "The reality-bending adventures of a clandestine service agency in the year 2166",
+            "year": 2014,
+            "quantity": 10,
+            "imageURL": "https://imgur.com/LEqsHy5.jpeg"
+          },
+          {
+            "title": "Tess the Wonder Dog",
+            "description": "The tale of a dog who gets super powers",
+            "year": 2007,
+            "quantity": 3,
+            "imageURL": "https://imgur.com/cEJmGKV.jpg"
+          },
+          {
+            "title": "The Annals of Arathrae",
+            "description": "This anthology tells the intertwined narratives of six fairy tales.",
+            "year": 2016,
+            "quantity": 8,
+            "imageURL": "https://imgur.com/VGyUtrr.jpeg"
+          },
+          {
+            "title": "Wâˆ€RP",
+            "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
+            "year": 2010,
+            "quantity": 4,
+            "imageURL": "https://imgur.com/qYLKtPH.jpeg"
+          }])
+            .then(res.status(200).json({
+                message: 'Seed successful'
+            }))
+            .catch(res.status(400).json({
+                message: 'Seed unsuccessful'
+            }))
+    })
+
 // Index
 books.get('/', (req, res) => {
     Book.find()
@@ -11,6 +49,33 @@ books.get('/', (req, res) => {
         .catch(err => {
             res.status(400).json({
                 message: 'Error, could not get all books'
+            })
+        })
+})
+
+// Show
+books.get('/:id', (req, res) => {
+    Book.findById(req.params.id)
+        .then(foundBook => {
+            res.status(200).json(foundBook)
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: 'Error, could not find this book'
+            })
+        })
+})
+
+// Update
+books.put('/:id', (req, res) => {
+    Book.findByIdAndUpdate(req.params.id, req.body)
+        .then(updatedBook => {
+            console.log(req.body)
+            res.status(200).json(updatedBook)
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: 'Error, could not edit book'
             })
         })
 })
